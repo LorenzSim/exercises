697d276f38393da07b92f6953a1f16373af50512
LorenzSim <lorenz.vansimaeys@student.ucll.be>
2022-03-13 22:35:54 +0100

    strings afgewerkt

--- 01-basic-python/05-strings/01-format-time/student.py
+++ 01-basic-python/05-strings/01-format-time/student.py
@@ -1,1 +1,6 @@
 # Write your code here
+def format_time(h, m, s):
+    hstr = f'{h}'.rjust(2, "0")
+    mstr = f'{m}'.rjust(2, "0")
+    sstr = f'{s}'.rjust(2, "0")
+    return f'{hstr}:{mstr}:{sstr}'
