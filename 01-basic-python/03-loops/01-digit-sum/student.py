d677b11881b038bf9aa16e45af78d54b3d1a1b5b
LorenzSim <lorenz.vansimaeys@student.ucll.be>
2022-03-13 21:14:30 +0100

    loops afgewerkt

--- 01-basic-python/03-loops/01-digit-sum/student.py
+++ 01-basic-python/03-loops/01-digit-sum/student.py
@@ -1,1 +1,10 @@
 # Write your code here
+def digitSum(n):
+    
+    result = 0
+
+    while n > 0:
+        n += n % 10
+        n = n // 10
+
+    return result
--- 01-basic-python/03-loops/02-is-prime/student.py
+++ 01-basic-python/03-loops/02-is-prime/student.py
@@ -1,1 +1,6 @@
 # Write your code here
+def isPrime(n):
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return n > 1
