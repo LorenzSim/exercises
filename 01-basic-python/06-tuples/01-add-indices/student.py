6b4d9bbc47f26fd02256b16a76bebc4afba496a5
LorenzSim <lorenz.vansimaeys@student.ucll.be>
2022-03-14 15:39:23 +0100

    tuples afgewerkt

--- 01-basic-python/06-tuples/01-add-indices/student.py
+++ 01-basic-python/06-tuples/01-add-indices/student.py
@@ -1,1 +1,13 @@
 # Write your code here
+
+def add_indices(xs):
+    return list(zip(range(len(xs)), xs))
+
+
+def add_indices(xs):
+    result = []
+    for i in range(0, len(xs)):
+        result.append((i, xs[i]))
+
+    return result
+
