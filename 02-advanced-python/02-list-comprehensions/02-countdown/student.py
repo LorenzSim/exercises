9f93c9c37f08161d3e691e485320d8178910a842
LorenzSim <lorenz.vansimaeys@student.ucll.be>
2022-03-23 16:33:22 +0100

    advanced python 0-3

--- 02-advanced-python/02-list-comprehensions/02-countdown/student.py
+++ 02-advanced-python/02-list-comprehensions/02-countdown/student.py
@@ -1,1 +1,5 @@
 # Write your code here
+
+
+def countdown(n):
+    return ', '.join([str(i) for i in sorted(range(1, n + 1), reverse=True)])
--- 02-advanced-python/02-list-comprehensions/03-slug/student.py
+++ 02-advanced-python/02-list-comprehensions/03-slug/student.py
@@ -1,1 +1,9 @@
 # Write your code here
+
+def slug(string):
+    words = ([w.lower() for w in string.split(' ')])
+    first = words[0]
+    rest = words[1:]
+    rest.extend(first)
+    return '-'.join(rest)
+    
