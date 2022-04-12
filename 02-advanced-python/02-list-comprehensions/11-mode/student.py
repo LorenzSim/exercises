aa81ceb6a4e47b99d747834d11df3b5a5fd176ff
LorenzSim <lorenz.vansimaeys@student.ucll.be>
2022-04-12 21:13:05 +0200

    update

--- 02-advanced-python/02-list-comprehensions/11-mode/student.py
+++ 02-advanced-python/02-list-comprehensions/11-mode/student.py
@@ -1,8 +1,4 @@
 # Write your code here
-
-"""def mode(ns):
-    counts = [ns.count(n) for n in set(ns)]
-    return ns[counts.index(max(counts))]"""
 def frequencies(ns):
     result = {}
     for n in ns:
@@ -10,8 +6,6 @@ def frequencies(ns):
             result[n] = 0
         result[n] += 1
     return result
-def mode(ns):
-    fs = frequencies(ns)
-    return max(fs.items(), key=second)[0]
-def second(pair):
-    return pair[1]
+
+def mode(ns): return max(frequencies(ns).items(), key = lambda a : a[1])[0]
+
--- 02-advanced-python/02-list-comprehensions/12-greatest-sum/student.py
+++ 02-advanced-python/02-list-comprehensions/12-greatest-sum/student.py
@@ -1,1 +1,16 @@
 # Write your code here
+def greatest_sum(ns): 
+    return max([(i, j) for i in range(len(ns)) for j in range(i + 1, len(ns) + 1)], key=lambda pairs : sum(ns[pairs[0]:pairs[1]]))
+
+def greatest_sum2(ns):
+    max = 0
+    res = (0, 1)
+    for i in range(len(ns)):
+        for j in range(i + 1, len(ns) + 1):
+            tmp = sum(ns[i:j])
+            if (tmp > max):
+                res = (i, j)
+                max = tmp
+    return res    
+
+    
